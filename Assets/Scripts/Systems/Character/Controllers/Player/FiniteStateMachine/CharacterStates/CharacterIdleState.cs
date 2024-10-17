b6f890a1f331a3f0b64293dba463120d084b5d01
SonwardGameMaker <sonward.gamemaker@gmail.com>
2024-10-17 20:52:46 +0300

    a

--- Assets/Scripts/Systems/Character/Controllers/Player/FiniteStateMachine/CharacterStates/CharacterIdleState.cs
+++ Assets/Scripts/Systems/Character/Controllers/Player/FiniteStateMachine/CharacterStates/CharacterIdleState.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 internal class CharacterIdleState : CharacterState
 {
     public CharacterIdleState(
-        CharacterBlank character,
+        GridManager gridManager,
         CharacterIngameController characterController,
-        CharacterStateMachine stateMachine) : base(character, characterController, stateMachine)
+        IInputHandler inputHandler,
+        CharacterStateMachine stateMachine) : base(gridManager, characterController, inputHandler, stateMachine)
     {
 
+    }
+
+    public override void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExitState()
+    {
+        throw new System.NotImplementedException();
     }
 }
